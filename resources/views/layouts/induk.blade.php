c783c605b154ee6a862ed183a1835c602002e776
iKursus <ikursus.com@gmail.com>
2022-10-26 13:08:23 +0800

    Latest update

--- resources/views/layouts/induk.blade.php
+++ resources/views/layouts/induk.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Blank</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('/themes/sbadmin2') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
